2a1a3a556795533440ae1221297bbd47acd8a7bc
HARIHARAN B P <bphariharan1301@gmail.com>
2022-06-03 13:53:50 +0530

    Feat: Export feature added

--- backend/pages/admin.py
+++ backend/pages/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
 from pages.models import Guide
+from import_export.admin import ExportActionMixin
+# from import_export.admin import ExportActionMixin
 
 # Register your models here.
 
-admin.site.register(Guide)
+
+class GuideAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('name', 'domain_1', 'domain_2', 'domain_3',
+                    'email', 'experience', 'vacancy')
+
+
+admin.site.register(Guide, GuideAdmin)

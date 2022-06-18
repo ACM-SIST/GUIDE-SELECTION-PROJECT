d0d504164343e9b3d2df2ec231b58ba67e95d9fa
HARIHARAN B P <bphariharan1301@gmail.com>
2022-06-18 22:06:22 +0530

    Feat: added guide table

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -114,22 +114,22 @@ def project_details(request):
 
     if request.method == 'POST':
 
-        project_name = request.POST['project_name']
-        project_domain = request.POST['project_domain']
-        project_description = request.POST['project_description']
-        no_of_members = request.POST['no_of_members']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        reg_no_1 = request.POST['reg_no_1']
-        student_1_email = request.POST['student_1_email']
-        student_1_no = request.POST['student_1_no']
+        project_details.project_name = request.POST['project_name']
+        project_details.project_domain = request.POST['project_domain']
+        project_details.project_description = request.POST['project_description']
+        project_details.no_of_members = request.POST['no_of_members']
+        project_details.first_name = request.POST['first_name']
+        project_details.last_name = request.POST['last_name']
+        project_details.reg_no_1 = request.POST['reg_no_1']
+        project_details.student_1_email = request.POST['student_1_email']
+        project_details.student_1_no = request.POST['student_1_no']
 
-        if no_of_members == '2':
-            first_name_2 = request.POST['first_name']
-            last_name_2 = request.POST['last_name']
-            reg_no_2 = request.POST['reg_no_1']
-            student_2_email = request.POST['student_1_email']
-            student_2_no = request.POST['student_1_no']
+        if project_details.no_of_members == '2':
+            project_details.first_name_2 = request.POST['first_name']
+            project_details.last_name_2 = request.POST['last_name']
+            project_details.reg_no_2 = request.POST['reg_no_1']
+            project_details.student_2_email = request.POST['student_1_email']
+            project_details.student_2_no = request.POST['student_1_no']
 
         return redirect('select-guide')
     else:

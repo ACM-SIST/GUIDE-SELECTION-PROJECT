24abcd80d907361b7f64f202ad1d3fd1318510ed
Jagadeesh N <jagadeeshshirs6453@gmail.com>
2022-06-24 11:04:49 +0530

    credits

--- Front-end/Home/home.js
+++ Front-end/Home/home.js
@@ -1,11 +1,12 @@
 function terms_changed(termsCheckBox){
     const btn = document.getElementById('submit_button');
+    btn.style.backgroundColor = 'lightslategray';
+    btn.style.color = 'white';
     if(termsCheckBox.checked){
         document.getElementById("submit_button").disabled = false;
         btn.style.backgroundColor = '#831238';
         btn.style.color = 'white';
     } else{
         document.getElementById("submit_button").disabled = true;
-        
     }
 }
